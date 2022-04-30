525c538b03528c1851a1a98b8b832903053a586f
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-04-30 23:59:12 +0900

    220430 : Chat Completed

--- src/server.js
+++ src/server.js
@@ -15,13 +15,19 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({server});
 
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
-    socket.on("close", () => console.log("DisConnected from Server ❌"));
+    socket.on("close", onSocketClose);
     socket.on("message", (message) => {
-        console.log(message.toString());
+        sockets.forEach((aSocket) => aSocket.send(message));
     });
-    socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
