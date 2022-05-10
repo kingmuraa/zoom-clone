14cd89a8c2df79be7462d33477b1d8d65750d905
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-05-11 07:49:55 +0900

    220511 : Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,11 +3,26 @@ const socket = io()
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind=== "videoinput");
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        });
+    } catch (e) {
+            console.log(e);
+        }
+    }
 
 async function getMedia(){
     try {
@@ -16,7 +31,7 @@ async function getMedia(){
         video: true,
         });
         myFace.srcObject = myStream;
-        console.log(myStream);
+        await getCameras();
     } catch (e) {
         console.log(e);
     }
@@ -24,7 +39,25 @@ async function getMedia(){
 
 getMedia();
 
+function handleMuteClick() {
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
+
+    if(!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "mute";
+        muted = false;
+    }
+}
+
 function handleCameraClick() {
+    myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
+
     if(cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = true;
@@ -34,15 +67,6 @@ function handleCameraClick() {
     }
 }
 
-function handleMuteClick() {
-    if(!muted) {
-        muteBtn.innerText = "Unmute";
-        muted = true;
-    } else {
-        muteBtn.innerText = "mute";
-        muted = false;
-    }
-}
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
